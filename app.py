9297d6dcc05c8119bc69b44dd3aeac386ed52eb5
DA <dorina.ababii@gmail.com>
2024-10-20 21:02:32 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return f'File {filename} uploaded successfully!', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
